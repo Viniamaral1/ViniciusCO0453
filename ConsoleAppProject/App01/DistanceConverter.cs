e7c3966953600acdddea326d92436dad558d7f7f
Vinicius amaral <vi.ni.amaral@hotmail.com>
2022-03-08 13:19:19 +0000

    feature 2 and 3 completed

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -9,11 +9,48 @@ namespace ConsoleAppProject.App01
     /// Vinicius Do amaral
     /// </author>
     public class DistanceConverter
+     
     {
+        private double miles;
+
+
+        private double feet;
+
+        public bool Value { get; private set; }
+
         public void Run()
         {
             Outputheading();
+
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+
         
+        }
+
+        private void OutputFeet()
+        {
+            Console.WriteLine();
+            Console.WriteLine("  "  +  miles + " miles is "  + feet + " feet ");
+            Console.WriteLine();
+        }
+
+        private void CalculateFeet()
+        {
+            feet = miles * 5280;
+        }
+
+        /// <summary>
+        /// Output a message to the user to enter the miles
+        /// and read it in as a string and convert it to a double
+        /// </summary>
+
+        private void InputMiles()
+        {
+            Console.Write(" Enter the distance in miles >  ");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
         }
 
         private void Outputheading()
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleAppProject
     /// to start App01 to App05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 05/02/2022
+    /// Vinicius Do Amaral 08/03/2022
     /// </summary>
     public static class Program
     {
@@ -24,7 +24,7 @@ namespace ConsoleAppProject
             Console.WriteLine();
             Console.WriteLine(" =================================================");
             Console.WriteLine("    BNU CO453 Applications Programming 2021-2022! ");
-            Console.WriteLine("        by Derek                                  ");
+            Console.WriteLine("        by Vinicius                                  ");
             Console.WriteLine(" =================================================");
             Console.WriteLine();
 
